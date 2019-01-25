0ce974848b7682217321c3eb4a8e0a34b8890beb
Marco <bernardimarco52@gmail.co>
2019-01-25 15:55:07 +0100

    Update Controllino_KIT.ino

--- Controllino_KIT.ino
+++ Controllino_KIT.ino
@@ -1,9 +1,36 @@
+int pinBtn = 5;
+int pinLedStart = 6;
+int pinLedEnd = 11;
+
 void setup() {
-  // put your setup code here, to run once:
+  for (int i = pinLedStart; i <= pinLedEnd; i++){
+    pinMode(i, OUTPUT);
+  }
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int i = pinLedStart; i <= pinLedEnd; i++){
+    digitalWrite(i, HIGH);
+    if (i > pinLedStart){
+          delay(100);
+          digitalWrite(i-1, LOW);
+    }
+    if (i != pinLedEnd){
+      delay(500);
+    } else {
+      delay(500);
+    }
+  }
+  for (int i = pinLedEnd - 1; i >= pinLedStart; i--){
+    digitalWrite(i, HIGH);
+    delay(100);
+    digitalWrite(i+1, LOW);
+    
+    if (i != pinLedStart){
+     delay(500);
+    }
+  } 
+  
 
 }
